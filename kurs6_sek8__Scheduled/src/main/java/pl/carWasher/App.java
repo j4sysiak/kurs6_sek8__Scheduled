9e81c25d1560e04e780d166283a011433fc54b3e
Jacek <Jacek@BERLIN>
2020-04-21 06:33:10 +0200

    c

--- kurs6_sek8__Scheduled/src/main/java/pl/carWasher/App.java
+++ kurs6_sek8__Scheduled/src/main/java/pl/carWasher/App.java
@@ -3,10 +3,12 @@ package pl.carWasher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //import com.carWasher.properties.CarWasherProperties;
 
 @SpringBootApplication
+@EnableScheduling
 //@EnableConfigurationProperties(value=CarWasherProperties.class)
 public class App {
 
